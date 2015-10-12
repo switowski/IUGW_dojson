a731b351cee31aa4b3291eb784916d38264302a3
Sebastian Witowski <witowski.sebastian@gmail.com>
2015-10-12 13:18:58 +0200

    Fixed entry points

--- setup.py
+++ setup.py
@@ -13,14 +13,8 @@ setup(name='IUGW_dojson',
       ],
       zip_safe=False,
       entry_points={
-            'IUGW_dojson.marc21': [
-                  'bd01x09x = IUGW_dojson.fields.default.bd01x09x',
-                  'bd2xx = IUGW_dojson.fields.default.bd2xx',
-                  'bd5xx = IUGW_dojson.fields.default.bd5xx',
-                  'bd69x = IUGW_dojson.fields.default.bd69x',
-                  'bd7xx = IUGW_dojson.fields.default.bd7xx',
-                  'bd8xx = IUGW_dojson.fields.default.bd8xx',
-                  'bd9xx = IUGW_dojson.fields.default.bd9xx',
+            'dojson.contrib.marc21': [
+                  'IUGW_bd9xx = IUGW_dojson.fields.default.bd9xx',
             ],
       },
       )
